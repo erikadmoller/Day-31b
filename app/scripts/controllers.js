e1d4953c03a939d2dfe3334888be127bd8116bbb
Erika Moller <erikamoller@gmail.com>
2015-02-26 11:23:29 -0600

    HW is complete but added answers to display on web page (before it only console logged the answer

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,20 +2,23 @@ angular.module('basic.controllers', ['basic.services'])
 .controller('BasicCtrl', function($scope, Random, Students, Dashing) {
 
 	$scope.testTest = function() {
-		$scope.placeholder = ['a', 'b', 'c', 'd', 'e', 'f'];
-		console.log(Random($scope.placeholder));
+		var placeholder = ['a', 'b', 'c', 'd', 'e', 'f'];
+		return Random(placeholder);
 	};
 	$scope.testTest();
+	$scope.bindModel = $scope.testTest();
 
 	$scope.studentPairs = function() {
-		$scope.students = ['Billy', 'Celine', 'Michael', 'Barbara', 'Jimmy', 'Ludacris']; 
-		console.log(Students($scope.students));
+		var students = ['Billy', 'Celine', 'Michael', 'Barbara', 'Jimmy', 'Ludacris']; 
+		return Students(students);
 	}
 	$scope.studentPairs();
+	$scope.studentModel = $scope.studentPairs();
 
 	$scope.nospacesString = function() {
-		$scope.stringy = "Have a nice day!";
-		console.log(Dashing($scope.stringy));
+		var stringy = "Have a nice day!";
+		return Dashing(stringy);
 	}
 	$scope.nospacesString();
+	$scope.stringModel = $scope.nospacesString();
 });
